c5b2097c81b9a1ea8e276956315a3a0410a7ea3f
Aaron Relph <aaron@imgonnarelph.com>
2018-02-15 19:22:44 -0800

    Add Max's PRs

---
+++ components/stream.js
@@ -0,0 +1,38 @@
+const FAKE_STREAM = [
+  {
+    title: 'Someone Great'
+  },
+  {
+    title: 'Let It Go'
+  },
+  {
+    title: 'Royals'
+  },
+  {
+    title: 'Aint it so'
+  },
+  {
+    title: 'Get Lucky!!!'
+  }
+]
+
+export default () => (
+  <div>
+    <ul>
+      { FAKE_STREAM.map(item => (
+        <li>{ item.title }</li>
+      ))}
+    </ul>
+
+    <style jsx>{`
+      li {
+        border-bottom: 1px solid gray;
+        list-style: none;
+        padding: 1rem;
+      }
+      li:last-child {
+        border: none;
+      }
+    `}</style>
+  </div>
+)
